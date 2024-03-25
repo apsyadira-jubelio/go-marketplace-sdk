992f867d65f120a98f1621ad3b51fe7fea95a76c
adwin-jubelio <adwin.nugroho@jubelio.com>
2024-03-25 13:42:52 +0700

    feat: update path get token

--- tiktok/auth.go
+++ tiktok/auth.go
@@ -46,7 +46,7 @@ func (s *AuthServiceOp) GetOldAuthURL(appKey, state string) (string, error) {
 }
 
 func (s *AuthServiceOp) GetAccessToken(appKey, appSecret, code, grantType string) (*GetAccessTokenResponse, error) {
-	path := fmt.Sprintf("/auth/token/get?app_key%s&app_secret=%s&auth_code=%s&grant_type=%s", appKey, appSecret, code, grantType)
+	path := fmt.Sprintf("/api/v2/auth/token/get?app_key%s&app_secret=%s&auth_code=%s&grant_type=%s", appKey, appSecret, code, grantType)
 	resp := new(GetAccessTokenResponse)
 	err := s.client.Get(path, nil, resp)
 	return resp, err
